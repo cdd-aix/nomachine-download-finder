1b351cb31adaf46fb85db1969bf91a328e81f794
Chris Dukes <chris.dukes.aix@gmail.com>
2018-02-17 20:25:48 -0500

    Logging as a full json feed

--- nomachine/nomachine/spiders/downloads.py
+++ nomachine/nomachine/spiders/downloads.py
@@ -1,25 +1,70 @@
 # -*- coding: utf-8 -*-
 import re
+import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+class DownloadItem(scrapy.Item):
+    title = scrapy.Field()
+    version = scrapy.Field()
+    package_type = scrapy.Field()
+    supported_os = scrapy.Field()
+    md5 = scrapy.Field()
+    url = scrapy.Field()
+    transform = {
+        'Package type:': 'package_type',
+        'MD5 signature:': 'md5',
+        'For:': 'supported_os',
+        'Version:': 'version',
+    }
 
 class DownloadsSpider(CrawlSpider):
     name = 'downloads'
     allowed_domains = ['www.nomachine.com']
     start_urls = [
         'https://www.nomachine.com/download',
+        'https://www.nomachine.com/download-enterprise'
     ]
 
     rules = (
         Rule(
-            LinkExtractor(
-                allow=(re.compile('/download/'),),
-            ),
+            LinkExtractor(allow=(re.compile('/download/.*&id='))),
             callback='parse_item',
             follow=True
         ),
     )
+    download='//%s[contains(@onclick, "/download.nomachine.com/")]/@onclick'
+    download_a = download % ('a',)
+    download_div = download % ('div',)
+    link='//%s[contains(@onclick, "&id=")]/@onclick'
+    link_a = link % ('a', )
+    link_div = link % ('div',)
 
     def parse_item(self, response):
-        print('Processing..' + response.url)
+        print('Processing ' + response.url)
+        # item_links = response.css('.linux_sub > .detailsLink::attr(onclick)').extract()
+        download_onclicks = response.xpath(self.download_a).extract()
+        download_onclicks += response.xpath(self.download_div).extract()
+        download_links = [a.split("'")[1] for a in download_onclicks]
+        for item in self.parse_downloads(response, download_links):
+            yield item
+
+        item_onclicks = response.xpath(self.link_a).extract()
+        item_onclicks += response.xpath(self.link_div).extract()
+        item_links = [a.split("'")[1] for a in item_onclicks]
+        for a in item_links:
+            yield response.follow(a, callback=self.parse_item)
+
+    def parse_downloads(self, response, downloads):
+        if downloads:
+            title = response.xpath('//h1[contains(@id, "titleH2")]/text()').extract()[0].strip()
+            attrs = dict(
+                zip(
+                    response.xpath('//div[@class="first_cell dis_cell"]/p/text()').extract(),
+                    response.xpath('//div[@class="sec_cell dis_cell"]/p/text()').extract()
+                )
+            )
+        for url in downloads:
+            item = DownloadItem(title=title, url=url)
+            item.update((item.transform[k],v) for k,v in attrs.items() if item.transform.get(k))
+            yield item
